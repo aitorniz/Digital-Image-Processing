3537ed8640918c107554907b52b4ca56892361fa
aitorniz <aitorlopez@orange.fr>
2024-11-20 14:58:26 +0100

    Imported dip02 homework file and set up inside a C++ compilation environment with CMake

---
+++ dip02/build/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/Dip2.cpp.o"
+  "CMakeFiles/code.dir/Dip2.cpp.o.d"
+  "libcode.a"
+  "libcode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
